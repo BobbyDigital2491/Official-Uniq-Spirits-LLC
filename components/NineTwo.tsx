95bf343d40139e7e93d84b8fe833cb6c5eb69c2c
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-09-04 12:19:29 -0400

    Create NineTwo.tsx

---
+++ components/NineTwo.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const NineTwo = () => {
+  return (
+    <div >
+<section className="bg-white dark:bg-white">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div className="max-w-screen-lg text-black sm:text-lg dark:text-black">
+          <p className="mb-4 font-light">Beyond the bottle, U&apos;NiQ Craft Wine is committed to
+           sharing our passion and knowledge with our community. We host engaging wine tastings and
+            educational events, where enthusiasts can immerse themselves in the world of wine and 
+            discover new favorites.</p>
+          <p className="mb-4 font-light">Whether you&apos;re a seasoned wine lover or just beginning your journey, U&apos;NiQ Craft Wine invites you to experience the magic of exceptional craftsmanship and unparalleled flavors. Join us as we raise a glass to celebrate life&apos;s unforgettable moments, one sip at a time.</p>
+          
+      </div>
+  </div>
+</section>
+
+    </div>
+  )
+}
+
+export default NineTwo
