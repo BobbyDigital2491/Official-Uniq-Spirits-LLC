daaba5ed585f14f8a50b3bf81798fcc9c657f61c
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-09-04 12:25:21 -0400

    Update AHero.tsx

--- components/AHero.tsx
+++ components/AHero.tsx
@@ -4,43 +4,25 @@ import React from 'react'
 const AHero = () => {
   return (
     <div>
-      
-        <section className='bg-black'>
-  <div
-    className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
-  >
-    <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-      <div
-        className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full"
-      >
-        <img
-          alt="Podcasts"
-          src="/Filmz.png"
-          className="absolute inset-0 h-full w-full object-cover"
-        />
-      </div>
-
-      <div className="lg:py-24">
-        <h2 className="text-3xl text-yellow-400 font-bold sm:text-4xl">About Us</h2>
-
-        <p className="mt-4 text-white">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic
-          atque tenetur quis eius quos ea neque sunt, accusantium soluta minus
-          veniam tempora deserunt? Molestiae eius quidem quam repellat.
-        </p>
-
-        <a
-          href="#"
-          className="mt-8 inline-block rounded bg-yellow-400 text-white px-12 py-3 text-sm font-medium transition hover:bg-white hover:text-black focus:outline-none focus:ring focus:ring-yellow-400"
-        >
-          Contact
-        </a>
-      </div>
-    </div>
-  </div>
-</section>
-
+      <section className="bg-white dark:bg-black">
+    <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <div className="font-light text-gray-500 sm:text-lg dark:text-white">
         
+        <span className="font-serif italic font-normal text-8xl text-transparent bg-clip-text bg-gradient-to-br from-yellow-300 to-yellow-900 ">About<span className='text-red-500'>Us</span></span>
+            <p className="mb-4">
+              Welcome to the world of U&apos;NiQ Craft Wine, where passion meets craftsmanship
+              in every bottle. We are a vibrant and dynamic winery dedicated to redefining the 
+              art of winemaking. With a blend of tradition and innovation, we strive to create 
+              an extraordinary experience for wine enthusiasts and connoisseurs alike.
+              </p>
+            
+        </div>
+        <div className="grid grid-cols-2 gap-4 mt-8">
+            <img className="w-full rounded-lg" src="Shop.jpg" alt="office content 1"/>
+            <img className="mt-4 w-full lg:mt-10 rounded-lg" src="Shelf.jpeg" alt="office content 2"/>
+        </div>
+    </div>
+</section>    
     </div>
   )
 }
