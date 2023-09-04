5e404d20077470f6fa6490aa42ae5ec6400ed32d
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-09-04 12:18:03 -0400

    Create NineOne.tsx

---
+++ components/NineOne.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+
+const NineOne = () => {
+  return (
+    <div >
+<section className="bg-white dark:bg-white">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div className="max-w-screen-lg text-black sm:text-lg dark:text-black">
+      <br/>
+          <p className="mb-4 font-light">At U&apos;NiQ Craft Wine, we believe that great wine is an
+           expression of creativity and attention to detail. Our talented team of winemakers 
+           meticulously handcrafts each bottle, ensuring that every sip is a delightful journey
+            for your taste buds. We carefully source the finest grapes from renowned vineyards, 
+            embracing the diversity of flavors and terroir to create truly exceptional wines.</p>
+          <p className="mb-4 font-medium">What sets us apart is our relentless pursuit of uniqueness.
+           We love pushing boundaries and experimenting with unconventional blends, resulting in wines
+            that surprise and captivate. From bold and robust reds to crisp and refreshing whites, 
+            our portfolio offers a tapestry of flavors that cater to every palate.</p>
+          
+      </div>
+  </div>
+</section>
+
+    </div>
+  )
+}
+
+export default NineOne
