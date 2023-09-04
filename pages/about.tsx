ef376b0640023426d4a1b70f7f18153f6eac045b
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-09-04 12:17:15 -0400

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,20 +3,24 @@ import React from 'react'
 import Nav from '../components/Nav'
 import Seo from '../components/Seo'
 import About from '../components/About'
+import NineOne from '../components/NineOne'
+import NineTwo from '../components/NineTwo'
+import Gallery from '../components/Gallery'
 import Footer from '../components/Footer'
-import Info3 from '../components/Info3'
 import AHero from '../components/AHero'
 
 const about = () => {
   return (
     <div className='bg-black'>
-      <Seo title={"Channel 91"} 
-      description={"Welcome to Channel 91, a variety channel where you can find whatever content your'e looking for. From documentaries to podcasts, comedy skits to real news. Channel 91 is the home of content."} 
-      image={"public/monkey.png"} 
-      url={"CH91.com"}  />
-      <Nav/>
+      <Seo title={"U'NiQ Spirits"} 
+      description={"About Us."} 
+      image={"UNIQ.jpg"} 
+      url={"drinkuniq.com"}  />
+      <Nav2/>
       <AHero/>
-      
+      <NineOne/>
+      <Gallery/>
+      <NineTwo/>
       <Footer/>
       
     </div>
